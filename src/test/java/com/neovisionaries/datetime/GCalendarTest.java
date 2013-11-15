ab4141d1de3e1f4812cb5be8c948f38ecce27dc7
Takahiko Kawasaki <takahiko.kawasaki@neovisionaries.com>
2013-11-15 16:32:28 +0900

    Added some tests for GCalendar#toISO8601().

--- src/test/java/com/neovisionaries/datetime/GCalendarTest.java
+++ src/test/java/com/neovisionaries/datetime/GCalendarTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (C) 2012 Neo Visionaries Inc.
- * 
+ * Copyright (C) 2012-2013 Neo Visionaries Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,9 +16,11 @@
 package com.neovisionaries.datetime;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.Calendar;
 import java.util.TimeZone;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 
 public class GCalendarTest
@@ -70,5 +72,27 @@ public class GCalendarTest
 
         // Zone offsets should be different.
         assertTrue(beforeOffset != afterOffset);
+    }
+
+
+    @Test
+    public void test2()
+    {
+        GCalendar cal = new GCalendar(JST(), 1974, Calendar.MAY, 6, 12, 34, 56, 0);
+        String actual = cal.toISO8601();
+        String expected = "1974-05-06T12:34:56+09:00";
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void test3()
+    {
+        GCalendar cal = new GCalendar(GMT(), 1974, Calendar.MAY, 6, 12, 34, 56, 0);
+        String actual = cal.toISO8601();
+        String expected = "1974-05-06T12:34:56+00:00";
+
+        assertEquals(expected, actual);
     }
 }
